8504811c72e95bd0ff87c8695c43cf2405e7f41f
Shaun <shaun@thenetninja.co.uk>
2020-07-14 19:11:08 +0100

    lesson-4

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,5 @@
+const User = require("../models/User");
+
 // controller actions
 module.exports.signup_get = (req, res) => {
   res.render('signup');
@@ -10,8 +12,15 @@ module.exports.login_get = (req, res) => {
 module.exports.signup_post = async (req, res) => {
   const { email, password } = req.body;
 
-  console.log(email, password);
-  res.send('new signup');
+  try {
+    const user = await User.create({ email, password });
+    res.status(201).json(user);
+  }
+  catch(err) {
+    console.log(err);
+    res.status(400).send('error, user not created');
+  }
+ 
 }
 
 module.exports.login_post = async (req, res) => {
