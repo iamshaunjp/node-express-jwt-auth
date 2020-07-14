d87bf905970bd97a341ad664e987d12da3d6b78d
Shaun <shaun@thenetninja.co.uk>
2020-07-14 16:30:42 +0100

    lesson-3

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = express();
 
 // middleware
 app.use(express.static('public'));
+app.use(express.json());
 
 // view engine
 app.set('view engine', 'ejs');
--- controllers/authController.js
+++ controllers/authController.js
@@ -8,9 +8,15 @@ module.exports.login_get = (req, res) => {
 }
 
 module.exports.signup_post = async (req, res) => {
+  const { email, password } = req.body;
+
+  console.log(email, password);
   res.send('new signup');
 }
 
 module.exports.login_post = async (req, res) => {
+  const { email, password } = req.body;
+
+  console.log(email, password);
   res.send('user login');
 }
