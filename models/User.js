f0535958802f56faf04127f0d156b8ba6d6f75bc
Shaun <shaun@thenetninja.co.uk>
2020-07-15 12:42:28 +0100

    lesson-7

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const { isEmail } = require('validator');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -16,15 +17,11 @@ const userSchema = new mongoose.Schema({
   }
 });
 
-// fire a function after doc saved to db
-userSchema.post('save', function (doc, next) {
-  console.log('new user was created & saved', doc);
-  next();
-});
 
 // fire a function before doc saved to db
-userSchema.pre('save', function (next) {
-  console.log('user about to be created & saved', this);
+userSchema.pre('save', async function(next) {
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
   next();
 });
 
