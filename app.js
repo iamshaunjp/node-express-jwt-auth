9b5812ff0e142d9d6dee88f69c3c0160509c7fdb
Shaun <shaun@thenetninja.co.uk>
2020-07-15 15:46:57 +0100

    lesson-9

--- app.js
+++ app.js
@@ -21,3 +21,27 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCr
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
 app.use(authRoutes);
+
+// cookies
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
+
+app.get('/set-cookies', (req, res) => {
+
+  // res.setHeader('Set-Cookie', 'newUser=true');
+  
+  res.cookie('newUser', false);
+  res.cookie('isEmployee', true, { maxAge: 1000 * 60 * 60 * 24, httpOnly: true });
+
+  res.send('you got the cookies!');
+
+});
+
+app.get('/read-cookies', (req, res) => {
+
+  const cookies = req.cookies;
+  console.log(cookies.newUser);
+
+  res.json(cookies);
+
+});
