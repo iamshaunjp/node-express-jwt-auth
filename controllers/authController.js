8f6452a0a8cdba20638dbee4e3fbf6b9b8dee65f
Shaun <shaun@thenetninja.co.uk>
2020-07-16 16:16:31 +0100

    lesson-14

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,6 +6,16 @@ const handleErrors = (err) => {
   console.log(err.message, err.code);
   let errors = { email: '', password: '' };
 
+  // incorrect email
+  if (err.message === 'incorrect email') {
+    errors.email = 'That email is not registered';
+  }
+
+  // incorrect password
+  if (err.message === 'incorrect password') {
+    errors.password = 'That password is incorrect';
+  }
+
   // duplicate email error
   if (err.code === 11000) {
     errors.email = 'that email is already registered';
@@ -63,9 +73,13 @@ module.exports.login_post = async (req, res) => {
 
   try {
     const user = await User.login(email, password);
+    const token = createToken(user._id);
+    res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
     res.status(200).json({ user: user._id });
-  } catch (err) {
-    res.status(400).json({});
+  } 
+  catch (err) {
+    const errors = handleErrors(err);
+    res.status(400).json({ errors });
   }
 
 }
