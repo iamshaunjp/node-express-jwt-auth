eeaf82c6a1b18b3f7a95408141728c26652cd5ce
EnYang999 <146144774+EnYang999@users.noreply.github.com>
2023-11-16 06:42:25 -0500

    update

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
 // middleware
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // view engine
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 // database connection
-const dbURI = 'mongodb+srv://shaun:test1234@cluster0.del96.mongodb.net/node-auth';
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
-  .then((result) => app.listen(3000))
-  .catch((err) => console.log(err));
+const dbURI =
+	"mongodb+srv://BookLib:SAZ4VN2fjJTL4Cl7@cluster0.tweiseh.mongodb.net/?retryWrites=true&w=majority";
+mongoose
+	.connect(dbURI, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useCreateIndex: true,
+	})
+	.then((result) => app.listen(3000))
+	.catch((err) => console.log(err));
 
 // routes
-app.get('/', (req, res) => res.render('home'));
-app.get('/smoothies', (req, res) => res.render('smoothies'));
+app.get("/", (req, res) => res.render("home"));
+app.get("/smoothies", (req, res) => res.render("smoothies"));
